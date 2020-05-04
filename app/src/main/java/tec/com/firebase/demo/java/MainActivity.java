c40cac6265c7d3a1294e5f2a9d1d22aa28458d65
mehul4795 <mehul4795@gmail.com>
2020-05-04 23:21:59 +0530

    Added code for the Save and retrieve data from Firebase post.

--- app/src/main/java/tec/com/firebase/demo/java/MainActivity.java
+++ app/src/main/java/tec/com/firebase/demo/java/MainActivity.java
@@ -1,14 +1,70 @@
 package tec.com.firebase.demo.java;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button submit;
+    DatabaseReference rootRef, demoRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.etValue);
+        submit = findViewById(R.id.btnSubmit);
+
+        // Database reference pointing to root of database
+        rootRef = FirebaseDatabase.getInstance().getReference();
+
+        // Database reference pointing to demo node
+        demoRef = rootRef.child("demo");
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value = editText.getText().toString();
+
+                // Push creates a unique id in database
+                demoRef.setValue(value);
+            }
+        });
+
+        Button fetchButton = findViewById(R.id.btnFetch);
+        final TextView fetchedText = findViewById(R.id.tvValue);
+
+        fetchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                demoRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String value = dataSnapshot.getValue(String.class);
+                        fetchedText.setText(value);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Toast.makeText(MainActivity.this, "Error fetching data", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
     }
 }
